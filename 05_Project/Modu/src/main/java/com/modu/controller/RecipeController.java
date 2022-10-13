81c97dad8eca84109b219043a222301fc12baa9b
KKM353 <dlqkftkdwk13@naver.com>
2022-10-13 16:05:52 +0900

    feat: 레시피등록 페이지
    insert 한줄씩 입력 가능

--- 05_Project/Modu/src/main/java/com/modu/controller/RecipeController.java
+++ 05_Project/Modu/src/main/java/com/modu/controller/RecipeController.java
@@ -1,12 +1,32 @@
 package com.modu.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.modu.domain.recipe.Direction;
+import com.modu.domain.recipe.Ingredient;
+import com.modu.domain.recipe.Recipe;
+import com.modu.domain.recipe.RecipeTag;
+import com.modu.service.RecipeFindingService;
+import com.modu.service.RecipeRegisterService;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
 @Controller
 @RequestMapping("recipe")
 public class RecipeController {
+	
+	@Autowired
+	private RecipeRegisterService recipeRegisterServcie;
+	
 	@GetMapping("/list")
 	public String recipeList() {
 		return "recipe/list";
@@ -20,5 +40,11 @@ public class RecipeController {
 	@GetMapping("/write")
 	public String recipeWrite() {
 		return "recipe/write";
+	}
+	@ResponseBody
+	@PostMapping("/write")
+	public String submit(HttpServletRequest request, HttpSession session) {
+		recipeRegisterServcie.registerRecipe(request, session);		
+		return "redirect:/";
 	}
 }
--- 05_Project/Modu/src/main/java/com/modu/service/RecipeRegisterService.java
+++ 05_Project/Modu/src/main/java/com/modu/service/RecipeRegisterService.java
@@ -1,8 +1,17 @@
 package com.modu.service;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.modu.domain.recipe.Direction;
+import com.modu.domain.recipe.Ingredient;
+import com.modu.domain.recipe.Recipe;
+import com.modu.domain.recipe.RecipeTag;
+
 public interface RecipeRegisterService {
 	/* 레시피 등록,수정,삭제와 관련된 기능 중심으로 인터페이스 작성 */
-	void addRecipe();
-	void modifyRecipe();
-	void deleteRecipe();
+	//void addRecipe();
+	//void modifyRecipe();
+	//void deleteRecipe();
+	void registerRecipe(HttpServletRequest request, HttpSession session);
 }
